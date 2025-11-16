b106dcf97447ab17cf5e46a1936ba20ddeefff83
Maulidya Isnaini <163633669+mauldynan@users.noreply.github.com>
2025-11-16 18:30:42 +0700

    Commit No. 3: Integrate Room Database for Data Persistence

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("org.jetbrains.kotlin.kapt")
 }
 
 android {
@@ -48,4 +49,9 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
 
     implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+
+    val roomVersion = "2.6.0"
+    implementation ("androidx.room:room-runtime:$roomVersion")
+    annotationProcessor("androidx.room:room-compiler:$roomVersion")
+    kapt ("androidx.room:room-compiler:$roomVersion")
 }
